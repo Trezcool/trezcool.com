7e636bc32851e5c62947b1995bc82d54aa7c66a0
Tresor Kambembo <kambembotresor@gmail.com>
2020-09-26 09:59:32 +0200

    switch to SVG 😎

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,8 @@
 <template>
   <Layout>
-    <h1>Hello there!</h1>
+<!--    <h1>Hello there!</h1>-->
 
-    <!-- FIXME: use SVG; fix Sharp's "svgload_buffer: SVG rendering failed" issue !!! -->
-    <g-image src="../assets/img/trezcode2.png" width="760" alt="TrezCode" />
+    <g-image src="../assets/img/trezcode.svg" width="760" alt="TrezCode" />
 
     <p>Hold tight! <strong>Trezcool</strong> is cooking something...</p>
   </Layout>
