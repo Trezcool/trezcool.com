9d9bbde18c158359a256256c287816227207d211
Tresor Kambembo <kambembotresor@gmail.com>
2020-09-26 06:17:52 +0200

    add home page image
    FIXME: switch to SVG after fixing Sharp issue..

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # trezcool.com
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/d0aacfb6-a2e0-42e2-ac48-374430d0a182/deploy-status)](https://trezcool.com)
+
+
+TODO: implement this guide: https://dev.to/github/10-standout-github-profile-readmes-h2o
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,10 @@
 <template>
   <Layout>
     <h1>Hello there!</h1>
+
+    <!-- FIXME: use SVG; fix Sharp's "svgload_buffer: SVG rendering failed" issue !!! -->
+    <g-image src="../assets/img/trezcode2.png" width="760" alt="TrezCode" />
+
     <p>Hold tight! <strong>Trezcool</strong> is cooking something...</p>
   </Layout>
 </template>
